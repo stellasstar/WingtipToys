3ac947a58e6c149baf0bda595b3dbe8f069e1957
stella silverstein <stellamarie@me.com>
2017-12-19 23:00:39 +0000

    Add project files.

---
+++ WingtipToysTests/App_Start/RouteConfigTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WingtipToys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WingtipToys.Tests
+{
+    [TestClass()]
+    public class RouteConfigTests
+    {
+        [TestMethod()]
+        public void RegisterRoutesTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ WingtipToysTests/App_Start/StartupTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WingtipToys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WingtipToys.Tests
+{
+    [TestClass()]
+    public class StartupTests
+    {
+        [TestMethod()]
+        public void ConfigureAuthTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
